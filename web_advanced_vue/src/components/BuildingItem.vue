713ab517f277edcd97560f5aeef3fc02c7dfea27
antonioFerroPXL <91885641+antonioFerroPXL@users.noreply.github.com>
2022-05-03 17:20:16 +0200

    Vue

---
+++ web_advanced_vue/src/components/BuildingItem.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="building-item">
+  <h2>{{ title }}</h2>
+  <label>Gold: {{gold}}</label>
+  <label>Resources needed: {{steel}} steel, {{ammo}} ammo</label>
+  <input type="button"
+         value="Build this building"
+         @click="buyBuilding">
+</div>
+</template>
+
+<script>
+export default {
+name: "BuildingItem",
+  data(){
+    return{
+      gold: 5000,
+      steel: 100,
+      ammo: 100
+    }
+  },
+  props:{
+    title: String
+  },
+  methods:{
+    buyBuilding(event){
+      alert(`${this.title} Building successfully bought!`)
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.building-item{
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+</style>
---
+++ web_advanced_vue/src/views/BuildingView.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Buildings</h1>
+  <!--<building-item v-bind:title="titleToPass"/>-->
+  <div v-for="building in buildings">
+    <building-item v-bind:title="building.title" :key="building.id"/>
+  </div>
+</template>
+
+<script>
+import BuildingItem from "@/components/BuildingItem";
+
+export default {
+  name: "BuildingView",
+  components: {
+    BuildingItem
+  },
+  data(){
+    return{
+      titleToPass: "Imperial Barracks",
+      buildings:[
+        {id: 1, title: "Imperial Barracks"},
+        {id: 2, title: "Astartes Barracks"},
+        {id: 3, title: "Psyker Barracks"}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
